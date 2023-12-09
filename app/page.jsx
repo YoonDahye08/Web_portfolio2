a58be4ba01537bbffdf2ed80448ff1c8240dcffd
YoonDahye08 <ydh_0801@naver.com>
2023-12-10 02:10:06 +0900

    1st

--- app/page.jsx
+++ app/page.jsx
@@ -31,20 +31,20 @@ export default async function Home() {
       <div>
         <h1 className="text-3xl mb-10">PORTFOLIO</h1>
         <div>
-          <Link href="https://webprogramming.vercel.app/" target="_blank"><Image src={'/image/project1.png'} width={520} height={100} className="ml-60" /></Link>
+          <Link href="https://webprogramming.vercel.app/" target="_blank"><Image src={'/image/project1.png'} width={520} height={100} className="ml-60" alt='image' /></Link>
           <Link href="https://github.com/YoonDahye08/webprogramming1" target="_blank">깃허브 바로가기</Link>
         </div>
         <div>
           <Link href="https://clerk2-nine.vercel.app/" target="_blank">
-            <Image src={'/image/project2.png'} width={520} height={100} className="ml-60 mt-10"/></Link>
+            <Image src={'/image/project2.png'} width={520} height={100} className="ml-60 mt-10" alt='image'/></Link>
           <Link href="https://github.com/YoonDahye08/clerk2" target="_blank">깃허브 바로가기</Link>
         </div>
         <div>
-          <Link href="https://dapara2-tawny.vercel.app/" target="_blank"><Image src={'/image/dapara.png'} width={520} height={100} className="ml-60 mt-10" /></Link>
+          <Link href="https://dapara2-tawny.vercel.app/" target="_blank"><Image src={'/image/dapara.png'} width={520} height={100} className="ml-60 mt-10" alt='image' /></Link>
           <Link href="https://github.com/YoonDahye08/Dapara2" target="_blank">깃허브 바로가기</Link>
         </div>
         <div>
-          <Link href="https://crud2-eight.vercel.app/" target="_blank"><Image src={'/image/crud.png'} width={520} height={100} className="ml-60 mt-10" /></Link>
+          <Link href="https://crud2-eight.vercel.app/" target="_blank"><Image src={'/image/crud.png'} width={520} height={100} className="ml-60 mt-10" alt='image' /></Link>
           <Link href="https://github.com/YoonDahye08/crud2" target="_blank">깃허브 바로가기</Link>
         </div>
       </div>
